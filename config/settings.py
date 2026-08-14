96f9817d3737f484538bf29916cc9cc582a62d4d
Ujjwal Singh <99257263+UjJwAl092003@users.noreply.github.com>
2026-08-14 23:16:24 +0530

    Add STATIC_ROOT for static file collection

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional static files directories (besides each app's own 'static' folder)
 STATICFILES_DIRS = [
